544ef576cbb6673b00d026f0b0c7ec6e212fb0f7
FraaaM <149014727+FraaaM@users.noreply.github.com>
2024-05-18 17:09:53 +0300

    Update simulated_annealing.py

--- practicum_5/homework/advanced/simulated_annealing.py
+++ practicum_5/homework/advanced/simulated_annealing.py
@@ -2,8 +2,6 @@ import numpy as np
 from numpy.typing import NDArray
 import networkx as nx
 
-from src.plotting import plot_graph, plot_loss_history
-
 
 NDArrayInt = NDArray[np.int_]
 
@@ -59,7 +57,7 @@ if __name__ == "__main__":
     seed = 42
     np.random.seed(seed)
     G = nx.erdos_renyi_graph(n=100, p=0.05, seed=seed)
-    plot_graph(G)
+    
 
     n_max_iters = 500
     n_max_colors = 3
@@ -68,5 +66,5 @@ if __name__ == "__main__":
     loss_history = solve_via_simulated_annealing(
         G, n_max_colors, initial_colors, n_max_iters
     )
-    plot_loss_history(loss_history)
+    
 
